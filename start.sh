19641ba27bb830065b1e08935fbc3c3386b782a7
Andrea Romanello aka rommel <rommelandrea@gmail.com>
2022-12-07 12:28:37 +0100

    improve the start.sh script

--- start.sh
+++ start.sh
@@ -1,12 +1,26 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+# setup kong
 docker-compose build kong
+sleep 2
 docker-compose up -d kong-db
+sleep 2
 docker-compose run --rm kong kong migrations bootstrap
+sleep 2
 docker-compose run --rm kong kong migrations up
+sleep 2
 docker-compose up -d kong
 docker-compose ps
 curl -s http://localhost:8001 | jq .plugins.available_on_server.oidc
+
+# setup konga and wait some time X to be sure that all services are up and running
 docker-compose up -d konga
 sleep 2m
+
+#setup keycloak
 docker-compose up -d keycloak-db
 docker-compose up -d keycloak
 docker-compose ps
